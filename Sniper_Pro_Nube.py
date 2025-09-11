b38faf94224e566a9b53d99f2882872bb15dd775
osalas29 <tecnieuro@gmail.com>
2025-09-10 20:48:06 -0400

    Add files via upload

--- Sniper_Pro_Nube.py
+++ Sniper_Pro_Nube.py
@@ -6,7 +6,6 @@ import numpy as np
 import csv
 from collections import Counter, deque
 from Programas_auxiliares import load_db, log_uso
-import construir_patrones
 
 ROJOS = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}
 NEGROS = set(range(1,37)) - ROJOS
