a848782ff0a6abcac0ff13340484de921ceb3b97
osalas29 <tecnieuro@gmail.com>
2025-09-10 21:12:48 -0400

    Add files via upload

--- Sniper_Pro_Nube.py
+++ Sniper_Pro_Nube.py
@@ -195,7 +195,7 @@ def main(page: ft.Page):
         else:  # Si no estábamos en predicción, entonces analizamos.
             manejar_estado_analizando()
 
-    def limpiar_secuencia():
+    def limpiar_secuencia(e=None):
         global estado_script, patron_activo, numeros_prediccion, giros_restantes, historial_numeros, historial_colores,\
             historial_combinado, historial_powerby
 
